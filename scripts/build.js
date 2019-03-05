33eef17dab8ac8c634eeee631fdcf8d3a3098cea
jiangzhifeng <jiangzhifeng@tigerbrokers.com>
2019-03-05 20:00:42 +0800

    init a webpack edit config

---
+++ scripts/build.js
@@ -0,0 +1,73 @@
+process.on("unhandledRejection", err => {
+  throw err;
+});
+
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const chalk = require("react-dev-utils/chalk");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+
+console.log(chalk.cyan("正在启动环境..."));
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "../src/index.js"),
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "../dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        exclude: /\.module\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.s[ac]ss$/,
+        exclude: /\.module\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    quiet: true,
+    overlay: true // 编译出现错误时，将错误直接显示在页面上
+  },
+  // Some libraries import Node modules but don't use them in the browser.
+  // Tell Webpack to provide empty mocks for them so importing them works.
+  node: {
+    module: "empty",
+    dgram: "empty",
+    dns: "mock",
+    fs: "empty",
+    net: "empty",
+    tls: "empty",
+    child_process: "empty"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Hello React",
+      template: path.resolve(__dirname, "../public/index.html")
+    }),
+    new FriendlyErrorsWebpackPlugin({
+      compilationSuccessInfo: {
+        messages: [
+          `Local: ${0 ? "https" : "http"}://localhost:3000`,
+          `On Your Network: ${0 ? "https" : "http"}://localhost:3000`
+        ]
+      },
+      onErrors: null,
+      clearConsole: true
+    })
+  ]
+};
